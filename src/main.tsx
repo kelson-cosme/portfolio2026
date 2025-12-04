f821210ec489a91eae9125f72d48e3a16ae256ff
kelson-cosme <kelson.almeida123@gmail.com>
2025-12-04 09:47:54 -0400

    GOOGLE TAGS

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async' // 1. Importar
 import './index.css'
 import App from './App.tsx'
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-6G4C2BDD4X");
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* 2. Envolver a App */}
